21bbeb88a7c8eaf44eb73cb87cb9d50cb34406ae
zWolv <samoht684@gmail.com>
2023-10-16 18:51:26 +0200

    health w/ scriptable objects

--- Assets/Scripts/Player/PlayerUI.cs
+++ Assets/Scripts/Player/PlayerUI.cs
@@ -2,20 +2,72 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+using Unity.VisualScripting;
 
 public class PlayerUI : MonoBehaviour
 {
-    [SerializeField] 
+    [SerializeField]
     private TextMeshProUGUI promptText;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    [SerializeField]
+    private PlayerHealthManagerScriptableObject healthManager;
 
+    private float health;
+    public Image frontHealthBar;
+    public Image backHealthBar;
+    public float CHIPSPEED = 2f;
+    private float lerpTimer;
+
+    public void Update()
+    {
+        UpdateHealthUI();
+    }
+
+    public void OnEnable()
+    {
+        healthManager.healthChangeEvent.AddListener(ChangeHealth);
+        health = healthManager.MaxHealth;
+    }
+
+    public void OnDisable()
+    {
+        healthManager.healthChangeEvent.RemoveListener(ChangeHealth);
     }
 
     public void UpdateText(string promptMessage)
     {
         promptText.text = promptMessage;
+    }
+
+    void UpdateHealthUI()
+    {
+        float fillF = frontHealthBar.fillAmount;
+        float fillB = backHealthBar.fillAmount;
+        float hFraction = health / healthManager.MaxHealth;
+        if (fillB > hFraction)
+        {
+            frontHealthBar.fillAmount = hFraction;
+            backHealthBar.color = Color.red;
+            lerpTimer += Time.deltaTime;
+            float percentComplete = lerpTimer / CHIPSPEED;
+            percentComplete *= percentComplete;
+            backHealthBar.fillAmount = Mathf.Lerp(fillB, hFraction, percentComplete);
+        }
+        if (hFraction > fillF)
+        {
+            backHealthBar.fillAmount = hFraction;
+            backHealthBar.color = Color.yellow;
+            lerpTimer += Time.deltaTime;
+            float percentComplete = lerpTimer / CHIPSPEED;
+            percentComplete *= percentComplete;
+            frontHealthBar.fillAmount = Mathf.Lerp(fillF, backHealthBar.fillAmount, percentComplete);
+        }
+    }
+
+    private void ChangeHealth(float health)
+    {
+        this.health = health;
+        lerpTimer = 0;
     }
 }
---
+++ Assets/Scripts/ScriptableObjectScripts/HealthManagerScriptableObject.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu(fileName = "PlayerHealthManagerScriptableObject", menuName = "ScriptableObjects/PlayerHealthManager")]
+public class PlayerHealthManagerScriptableObject : ScriptableObject
+{
+    [SerializeField] private float health = 100;
+    [SerializeField] public float MaxHealth { get { return 100; } private set { } }
+
+    [System.NonSerialized] public UnityEvent<float> healthChangeEvent;
+
+    private void OnEnable()
+    {
+        health = MaxHealth;
+        if (healthChangeEvent == null)
+        {
+            healthChangeEvent = new UnityEvent<float>();
+        }
+    }
+
+    public void DecreaseHealth(int amount)
+    {
+        health -= amount;
+        Mathf.Clamp(health, 0, MaxHealth);
+        healthChangeEvent.Invoke(health);
+    }
+
+    public void IncreaseHealth(int amount)
+    {
+        health += amount;
+        Mathf.Clamp(health, 0, MaxHealth);
+        healthChangeEvent.Invoke(health);
+    }
+}
